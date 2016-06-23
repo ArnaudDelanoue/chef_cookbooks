5b930993e3d9c0edd70e8a2f325ff1557c20f76c
Arnaud Delanoue <arnaud@pue.es>
2016-06-23 08:03:05 +0000

    Update default recipe to include setup recipe

---
+++ librojueves/README.md
@@ -0,0 +1,4 @@
+# librojueves
+
+TODO: Enter the cookbook description here.
+
---
+++ librojueves/metadata.rb
@@ -0,0 +1,7 @@
+name 'librojueves'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures librojueves'
+long_description 'Installs/Configures librojueves'
+version '0.1.0'
